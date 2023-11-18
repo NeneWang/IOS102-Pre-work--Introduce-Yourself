cbb8dae7a1941ee02f86456c40ce2f99404b159a
NelsonWang <wangnelson2@gmail.com>
2023-11-18 09:39:23 -0300

    Add gifs and refinement

--- IntrductionProject/ViewController.swift
+++ IntrductionProject/ViewController.swift
@@ -20,23 +20,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var YearSegmented: UISegmentedControl!
     @IBOutlet weak var NumberOfPets: UILabel!
 
+    @IBOutlet weak var MorePetsTxtField: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        MorePetsSwitch.addTarget(self, action: #selector(switchValueChanged), for: .valueChanged)
+
     }
 
     @IBAction func stepperDidChange(_ sender: UIStepper) {
         NumberOfPets.text = "\(Int(sender.value))"
     }
-
+  
+    @objc func switchValueChanged() {
+        if MorePetsSwitch.isOn {
+            MorePetsTxtField.textColor = UIColor.black
+        } else {
+            MorePetsTxtField.textColor = UIColor.gray 
+        }
+    }
     @IBAction func introduceSelfAlertDidTapped(_ sender: UIButton) {
+        
+        
         let year = YearSegmented.titleForSegment(at: YearSegmented.selectedSegmentIndex)
-
-        let introduction = "Hello, my name is \(FirstNameTC.text!) \(LastNameTC.text!) and I attend \(SchoolNameTC.text!). " +
+        
+        var introduction = "Hello, my name is \(FirstNameTC.text!) \(LastNameTC.text!) and I attend \(SchoolNameTC.text!). " +
             "I am currently in my \(year!) year. I own \(NumberOfPets.text!) dogs. " +
             "It is \(MorePetsSwitch.isOn) that I want more pets."
 
+        if(FirstNameTC.text == "" || LastNameTC.text == "" || SchoolNameTC.text == ""){
+            introduction = "Please complete the fields."
+        }
+            
         let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+    
 
         let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
 
